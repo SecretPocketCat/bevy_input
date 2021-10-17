365427941d197daf431be993fe8c0e80be0fc513
SecretPocketCat <jiridexterhanc@gmail.com>
2021-10-17 14:24:35 +0200

    inputs_vec test

--- src/macros.rs
+++ src/macros.rs
@@ -1,9 +1,22 @@
 #[macro_export]
 macro_rules! inputs_vec {
     ($($x:expr),+ $(,)?) => (
-        ::std::vec![$(bevy_input::ButtonCode::from($x)),+]
+        ::std::vec![$($crate::ButtonCode::from($x)),+]
     );
 }
 
 // todo: test coverage
 // test
+
+#[cfg(test)]
+mod tests {
+    use bevy::prelude::{GamepadButtonType, KeyCode, MouseButton};
+    use crate::ButtonCode;
+
+    #[test]
+    fn mixed_inputs() {
+        let actual = inputs_vec![MouseButton::Left, KeyCode::Left, GamepadButtonType::DPadLeft];
+        let expected = vec![ButtonCode::Mouse(MouseButton::Left), ButtonCode::Kb(KeyCode::Left), ButtonCode::Gamepad(GamepadButtonType::DPadLeft)];
+        assert_eq!(expected, actual);
+    }
+}
