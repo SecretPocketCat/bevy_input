05426c7e5bb05a8bd3a9e66ad61929c8e1fc19dd
SecretPocketCat <jiridexterhanc@gmail.com>
2022-03-04 22:12:25 +0100

    hodgepodge

--- src/plugin.rs
+++ src/plugin.rs
@@ -2,10 +2,23 @@ use bevy::{input::InputSystem, prelude::*};
 #[cfg(feature = "serialize")]
 use bevy_extensions::panic_on_error;
 
-use crate::{MapIoEvent, action_map::{
+#[cfg(feature = "serialize")]
+use crate::{
+    action_map::{
+        handle_gamepad_events, handle_keyboard_button_events, handle_mouse_button_events,
+        process_axis_actions, process_button_actions, ActionInput, ActionMap, ActionMapInput,
+    },
+    bindings_loader::{
+        load_map, process_map_event, save_map, ActionMapLoad, ActionMapSave, MapIoRequest,
+    },
+    MapIoEvent,
+};
+
+#[cfg(not(feature = "serialize"))]
+use crate::action_map::{
     handle_gamepad_events, handle_keyboard_button_events, handle_mouse_button_events,
     process_axis_actions, process_button_actions, ActionInput, ActionMap, ActionMapInput,
-}, bindings_loader::{ActionMapLoad, ActionMapSave, MapIoRequest, load_map, process_map_event, save_map}};
+};
 
 pub struct ActionInputPlugin<'a, TKeyAction, TAxisAction> {
     _key_t: std::marker::PhantomData<&'a TKeyAction>,
@@ -14,8 +27,8 @@ pub struct ActionInputPlugin<'a, TKeyAction, TAxisAction> {
 
 impl<'a, TKeyAction, TAxisAction> Default for ActionInputPlugin<'a, TKeyAction, TAxisAction> {
     fn default() -> Self {
-        Self { 
-            _key_t: Default::default(), 
+        Self {
+            _key_t: Default::default(),
             _axis_t: Default::default(),
         }
     }
@@ -24,8 +37,8 @@ impl<'a, TKeyAction, TAxisAction> Default for ActionInputPlugin<'a, TKeyAction, 
 impl<'a, TKeyAction, TAxisAction> Plugin for ActionInputPlugin<'a, TKeyAction, TAxisAction>
 where
     ActionMap<TKeyAction, TAxisAction>: Default,
-    TKeyAction: ActionMapInput + serde::Serialize + for<'de> serde::Deserialize<'de>,
-    TAxisAction: ActionMapInput + serde::Serialize + for<'de> serde::Deserialize<'de>,
+    TKeyAction: ActionMapInput,
+    TAxisAction: ActionMapInput,
     'a: 'static,
 {
     fn build(&self, app: &mut App) {
@@ -66,16 +79,13 @@ where
 
         #[cfg(feature = "serialize")]
         {
-            app
-                .insert_resource(ActionMapLoad::<TKeyAction, TAxisAction>(None))
+            app.insert_resource(ActionMapLoad::<TKeyAction, TAxisAction>(None))
                 .insert_resource(ActionMapSave(None))
                 .add_event::<MapIoRequest>()
                 .add_event::<MapIoEvent>()
                 .add_system(process_map_event::<TKeyAction, TAxisAction>)
-                .add_system(load_map::<TKeyAction, TAxisAction>
-                    .chain(panic_on_error))
-                .add_system(save_map
-                    .chain(panic_on_error));
+                .add_system(load_map::<TKeyAction, TAxisAction>.chain(panic_on_error))
+                .add_system(save_map.chain(panic_on_error));
         }
     }
 }
